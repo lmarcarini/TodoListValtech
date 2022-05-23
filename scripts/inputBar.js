6196663878133d711b05fb7476767502bdc1ccbc
Leonardo Marcarini Borges <leonardo.marcarini@yahoo.com.br>
2022-05-23 14:53:38 -0300

    Ajustado a centralização

--- scripts/inputBar.js
+++ scripts/inputBar.js
@@ -1,13 +1,9 @@
 const handleSubmit = (e, addTask) => {
     e.preventDefault();
     const task = new FormData(e.target).get("add-task");
-    if (task.length === 0) {
-        return;
-    }
-    if (task) {
-        addTask(task);
-        e.target.children.namedItem("add-task").value = '';
-    }
+    if (!task || task.length === 0) return;
+    addTask(task);
+    e.target.children.namedItem("add-task").value = '';
 }
 
 export { handleSubmit };
